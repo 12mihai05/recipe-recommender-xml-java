0e1ed86e1c48464f27a70e848f215d7b8a816d04
Pasaroiu Mihai <pasaroiumihai@yahoo.com>
2026-04-20 19:34:06 +0300

    Initial commit

---
+++ src/test/java/org/seweb/reciperecommenderxmljava/RecipeRecommenderXmlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seweb.reciperecommenderxmljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRecommenderXmlJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
